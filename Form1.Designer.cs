8e5172fe6d009d6c476ccc3586916fe5978a1a6e
d3n4ik.ermak@yandex.ru <d3n4ik.ermak@yandex.ru>
2023-10-08 14:40:17 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace RGRerm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGV = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.bt_run = new System.Windows.Forms.Button();
+            this.random_b = new System.Windows.Forms.Button();
+            this.result_b = new System.Windows.Forms.Button();
+            this.bt26 = new System.Windows.Forms.Button();
+            this.bt43 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt15 = new System.Windows.Forms.Button();
+            this.bt32 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGV
+            // 
+            this.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV.ColumnHeadersVisible = false;
+            this.DGV.Location = new System.Drawing.Point(46, 12);
+            this.DGV.Name = "DGV";
+            this.DGV.RowHeadersVisible = false;
+            this.DGV.Size = new System.Drawing.Size(630, 344);
+            this.DGV.TabIndex = 0;
+            this.DGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGV_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "столбцы";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 384);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "строки";
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(109, 384);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(100, 20);
+            this.tb1.TabIndex = 3;
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(109, 406);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(100, 20);
+            this.tb2.TabIndex = 4;
+            // 
+            // bt_run
+            // 
+            this.bt_run.Location = new System.Drawing.Point(277, 384);
+            this.bt_run.Name = "bt_run";
+            this.bt_run.Size = new System.Drawing.Size(174, 42);
+            this.bt_run.TabIndex = 5;
+            this.bt_run.Text = "сгенерировать матрицу";
+            this.bt_run.UseVisualStyleBackColor = true;
+            this.bt_run.Click += new System.EventHandler(this.bt_run_Click);
+            // 
+            // random_b
+            // 
+            this.random_b.Location = new System.Drawing.Point(698, 384);
+            this.random_b.Name = "random_b";
+            this.random_b.Size = new System.Drawing.Size(89, 41);
+            this.random_b.TabIndex = 1;
+            this.random_b.Text = "random generation";
+            this.random_b.UseVisualStyleBackColor = true;
+            this.random_b.Click += new System.EventHandler(this.random_b_Click);
+            // 
+            // result_b
+            // 
+            this.result_b.Location = new System.Drawing.Point(698, 217);
+            this.result_b.Name = "result_b";
+            this.result_b.Size = new System.Drawing.Size(75, 23);
+            this.result_b.TabIndex = 7;
+            this.result_b.Text = "9";
+            this.result_b.UseVisualStyleBackColor = true;
+            this.result_b.Click += new System.EventHandler(this.result_b_Click_1);
+            // 
+            // bt26
+            // 
+            this.bt26.Location = new System.Drawing.Point(698, 35);
+            this.bt26.Name = "bt26";
+            this.bt26.Size = new System.Drawing.Size(75, 23);
+            this.bt26.TabIndex = 8;
+            this.bt26.Text = "26";
+            this.bt26.UseVisualStyleBackColor = true;
+            this.bt26.Click += new System.EventHandler(this.bt26_Click);
+            // 
+            // bt43
+            // 
+            this.bt43.Location = new System.Drawing.Point(698, 122);
+            this.bt43.Name = "bt43";
+            this.bt43.Size = new System.Drawing.Size(75, 23);
+            this.bt43.TabIndex = 9;
+            this.bt43.Text = "43";
+            this.bt43.UseVisualStyleBackColor = true;
+            this.bt43.Click += new System.EventHandler(this.bt43_Click);
+            // 
+            // bt4
+            // 
+            this.bt4.Location = new System.Drawing.Point(698, 166);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(75, 23);
+            this.bt4.TabIndex = 10;
+            this.bt4.Text = "4";
+            this.bt4.UseVisualStyleBackColor = true;
+            this.bt4.Click += new System.EventHandler(this.bt4_Click);
+            // 
+            // bt15
+            // 
+            this.bt15.Location = new System.Drawing.Point(698, 81);
+            this.bt15.Name = "bt15";
+            this.bt15.Size = new System.Drawing.Size(75, 23);
+            this.bt15.TabIndex = 11;
+            this.bt15.Text = "15";
+            this.bt15.UseVisualStyleBackColor = true;
+            this.bt15.Click += new System.EventHandler(this.bt15_Click);
+            // 
+            // bt32
+            // 
+            this.bt32.Location = new System.Drawing.Point(698, 262);
+            this.bt32.Name = "bt32";
+            this.bt32.Size = new System.Drawing.Size(75, 23);
+            this.bt32.TabIndex = 12;
+            this.bt32.Text = "32";
+            this.bt32.UseVisualStyleBackColor = true;
+            this.bt32.Click += new System.EventHandler(this.bt32_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bt32);
+            this.Controls.Add(this.bt15);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt43);
+            this.Controls.Add(this.bt26);
+            this.Controls.Add(this.result_b);
+            this.Controls.Add(this.random_b);
+            this.Controls.Add(this.bt_run);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DGV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Button bt_run;
+        private System.Windows.Forms.Button random_b;
+        private System.Windows.Forms.Button result_b;
+        private System.Windows.Forms.Button bt26;
+        private System.Windows.Forms.Button bt43;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt15;
+        private System.Windows.Forms.Button bt32;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RGRerm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void DGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void random_b_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            DGV.RowCount = rnd.Next(2, 16);
+            DGV.ColumnCount = rnd.Next(2, 16);
+
+            for (int i = 0; i < DGV.RowCount; i++)
+            {
+                for (int j = 0; j < DGV.ColumnCount; j++)
+                {
+
+                    DGV[j, i].Value = rnd.Next(-100, 100) + Math.Round(rnd.NextDouble(), 2);
+                }
+
+            }
+
+
+        }
+
+        private void result_b_Click(object sender, EventArgs e)
+        {
+            double[,] Matr = new double[DGV.RowCount, DGV.ColumnCount];//создали массив
+            for (int i = 0; i < DGV.RowCount; i++)
+                for (int j = 0; j < DGV.ColumnCount; j++)
+                    Matr[i, j] = Convert.ToDouble(DGV[j, i].Value);
+
+            ////min max
+            double[] mas = new double[DGV.RowCount];
+
+            for (int i = 0; i < DGV.RowCount; i++)
+            {
+                double max = Matr[i, 0];
+                for (int j = 1; j < DGV.ColumnCount; j++)
+                {
+                    if (Matr[i, j] >= max)
+                        max = Matr[i, j];
+                }
+                mas[i] = max;
+            }
+            double minmax = mas[0];
+            for (int i = 1; i < mas.Length; i++)
+                if (minmax > mas[i])
+                    minmax = mas[i];
+            double p = minmax;
+
+
+
+            //maxmin
+            double[] mas1 = new double[DGV.ColumnCount];
+            for (int i = 0; i < DGV.ColumnCount; i++)
+            {
+                double min = Matr[0, i];
+                for (int j = 0; j < DGV.RowCount; j++)
+                {
+                    if (Matr[j, i] <= min)
+                        min = Matr[j, i];
+
+                }
+                mas1[i] = min;
+            }
+            double maxmin = mas1[0];
+            for (int i = 0; i < DGV.ColumnCount; i++)
+                if (maxmin < mas1[i])
+                    maxmin = mas1[i];
+            double q = maxmin;
+            MessageBox.Show($"minmax = {minmax}\nmaxmin = {maxmin}\n" + (p * q).ToString());
+            //MessageBox.Show((minmax,maxmin).ToString());
+        }
+
+
+
+        private void bt_run_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int.TryParse(tb1.Text, out int a);
+            int.TryParse(tb2.Text, out int b);
+            if (a != 0 && b != 0)
+            {
+                DGV.RowCount = a;
+                DGV.ColumnCount = b;
+
+                for (int i = 0; i < DGV.RowCount; i++)
+                {
+                    for (int j = 0; j < DGV.ColumnCount; j++)
+                    {
+
+                        DGV[j, i].Value = rnd.Next(-100, 100) + Math.Round(rnd.NextDouble(), 2);
+                    }
+
+                }
+            }
+            else
+                MessageBox.Show("введите ПРАВИЛЬНЫЕ числа");
+
+        }
+        //Сгенерировать квадратную(N≥5, M≥5) матрицу и заполнить вещественными числами(положительными и отрицательными). 
+        //    Отсортировать элементы побочной диагонали по убыванию.
+        private void bt26_Click(object sender, EventArgs e)
+        {
+            if (DGV.RowCount == DGV.ColumnCount && DGV.RowCount >= 5)
+            {
+                double[] tmp = new double[DGV.RowCount];
+
+                for (int i = 0; i < DGV.RowCount; i++)
+                {
+                    tmp[i] = Convert.ToDouble(DGV[i, DGV.RowCount - 1 - i].Value);
+                }
+                Array.Sort(tmp);
+                Array.Reverse(tmp);
+                for (int i = 0; i < DGV.RowCount; i++)
+                {
+                    DGV[i, DGV.RowCount - 1 - i].Value = tmp[i];
+                }
+            }
+            else
+                MessageBox.Show("какой то неправильный базар");
+        }
+        //Сгенерировать квадратную матрицу с четным количеством столбцов и заполнить вещественными числами(положительными и отрицательными). 
+        //    Поменять местами элементы главной и побочной диагонали в нечетных столбцах.
+        private void bt43_Click(object sender, EventArgs e)
+        {
+            if (DGV.RowCount == DGV.ColumnCount && DGV.RowCount % 2 == 0)
+            {
+                double[] tmpp = new double[DGV.RowCount / 2];
+                double[] tmpd = new double[DGV.RowCount / 2];
+
+                for (int i = 0; i < DGV.RowCount / 2; i++)
+                {
+                    tmpp[i] = Convert.ToDouble(DGV[i * 2, DGV.RowCount - 1 - i * 2].Value);
+                    tmpd[i] = Convert.ToDouble(DGV[i * 2, i * 2].Value);
+
+                }
+
+                for (int i = 0; i < DGV.RowCount / 2; i++)
+                {
+                    DGV[i * 2, DGV.RowCount - 1 - i * 2].Value = tmpd[i];
+                    DGV[i * 2, i * 2].Value = tmpp[i];
+
+                }
+            }
+            else
+                MessageBox.Show("неправильные значения....повторите ввод даных");
+
+        }
+        //Сгенерировать матрицу(N≥10, M≥10) и заполнить целыми положительными числами в диапазоне от 10 до 20.
+        // Определить значение, которые повторяются.Вывести все повторяющиеся значения и количество их повторений.
+        private void bt4_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            DGV.RowCount = rnd.Next(10, 16);
+            DGV.ColumnCount = rnd.Next(10, 16);
+
+            for (int i = 0; i < DGV.RowCount; i++)
+            {
+                for (int j = 0; j < DGV.ColumnCount; j++)
+                {
+
+                    DGV[j, i].Value = rnd.Next(10, 20);
+                }
+
+            }
+            int[] chisla = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (Convert.ToInt32(DGV[i, j].Value) == 10)
+                        chisla[0]++;
+                    if (Convert.ToInt32(DGV[i, j].Value) == 11)
+                        chisla[1]++;
+                    if (Convert.ToInt32(DGV[i, j].Value) == 12)
+                        chisla[2]++;
+                    if (Convert.ToInt32(DGV[i, j].Value) == 13)
+                        chisla[3]++;
+                    if (Convert.ToInt32(DGV[i, j].Value) == 14)
+                        chisla[4]++;
+                    if (Convert.ToInt32(DGV[i, j].Value) == 15)
+                        chisla[5]++;
+                    if (Convert.ToInt32(DGV[i, j].Value) == 16)
+                        chisla[6]++;
+                    if (Convert.ToInt32(DGV[i, j].Value) == 17)
+                        chisla[7]++;
+                    if (Convert.ToInt32(DGV[i, j].Value) == 18)
+                        chisla[8]++;
+                    if (Convert.ToInt32(DGV[i, j].Value) == 19)
+                        chisla[9]++;
+                }
+            }
+            MessageBox.Show($" 10- {chisla[0]}\n 11- {chisla[1]}\n 12- {chisla[2]}\n 13- {chisla[3]}\n 14- {chisla[4]}\n 15- {chisla[5]}\n 16- {chisla[6]}\n 17- {chisla[7]}\n 18- {chisla[8]}\n 19- {chisla[9]}\n");
+
+        }
+        //Сгенерировать матрицу(N != M) и заполнить вещественными числами(положительными и отрицательными).
+        //Реализовать алгоритм нахождения обратной матрицы и вывести обратную матрицу.
+        private void bt15_Click(object sender, EventArgs e)
+        {
+            if (DGV.RowCount == DGV.ColumnCount)
+            {
+                int n = DGV.RowCount;
+                double[,] inverseMatrix = new double[n, n];
+
+                double[,] augmentedMatrix = new double[n, 2 * n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        augmentedMatrix[i, j] = Convert.ToDouble(DGV[i, j].Value);
+                    }
+
+                    augmentedMatrix[i, i + n] = 1;
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    double pivot = augmentedMatrix[i, i];
+
+                    for (int j = 0; j < 2 * n; j++)
+                    {
+                        augmentedMatrix[i, j] /= pivot;
+                    }
+
+                    for (int k = 0; k < n; k++)
+                    {
+                        if (k != i)
+                        {
+                            double coefficient = augmentedMatrix[k, i];
+
+                            for (int j = 0; j < 2 * n; j++)
+                            {
+                                augmentedMatrix[k, j] -= coefficient * augmentedMatrix[i, j];
+                            }
+                        }
+                    }
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        DGV[i, j].Value = augmentedMatrix[i, j + n];
+                    }
+                }
+            }
+            else
+                MessageBox.Show("что то не то....введите новые значения");
+        }
+        //Сгенерировать матрицу(N≥10, M≥10) и заполнить целыми положительными числами в диапазоне от -2 до 2.
+        //    В каждом столбце найти количество нулевых значений и из них сформировать дополнительную строку матрицы.
+        private void bt32_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            DGV.RowCount = rnd.Next(10, 16);
+            DGV.ColumnCount = rnd.Next(10, 16);
+            int[] chisla = new int[DGV.ColumnCount];
+            for (int i = 0; i < DGV.RowCount; i++)
+            {
+                for (int j = 0; j < DGV.ColumnCount; j++)
+                {
+
+                    DGV[j, i].Value = rnd.Next(-2, 2);
+                    if (Convert.ToInt32(DGV[j, i].Value) == 0)
+                        chisla[j]++;
+                }
+            }
+
+
+            DGV.RowCount = DGV.RowCount;
+            for (int i = 0; i < DGV.ColumnCount; i++)
+            {
+                DGV[i, DGV.RowCount - 1].Value = chisla[i];
+
+            }
+        }
+
+        private void result_b_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
